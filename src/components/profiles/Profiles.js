e458c91f48046412e94db8560548d323f74cd54d
julioscalves <julioscalves@gmail.com>
2023-09-05 10:42:52 -0300

    fix: wrong comparison

--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -40,7 +40,7 @@ export default function Events() {
             </Link>
           </div>
         </div>
-        {data.length > 1 ? (
+        {data.length > 0 ? (
           data
             .reverse()
             .map((item) => <ProfileItem key={item.id} item={item} />)
