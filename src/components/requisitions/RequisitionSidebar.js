f75d92ec322d039a4cd4f67a1ed2b6af0e6faa2c
julioscalves <julioscalves@gmail.com>
2023-09-14 15:42:04 -0300

    improv: minor layout changes

--- src/components/requisitions/RequisitionSidebar.js
+++ src/components/requisitions/RequisitionSidebar.js
@@ -44,10 +44,10 @@ export default function RequisitionSidebar({
         )}
         <Sidebar.Items>
           <Sidebar.ItemGroup>
-            <Sidebar.Item>
+            <Sidebar.Item>S
               <div className="flex items-center justify-center">
                 <FaFilter className="mr-2" />
-                <p className="text-xl font-bold">Filtragem por tag</p>
+                <p className="text-xl font-bold">Filtragem por etiqueta</p>
               </div>
               <FilterTags
                 visualizationData={visualizationData}
